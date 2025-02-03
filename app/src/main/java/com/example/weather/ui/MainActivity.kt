d5dd5e366ab8f2dbb9733f2f78cf705fa4e7acef
Paxagi <paxagi@yandex.ru>
2025-02-03 11:31:36 +0300

    add: setup navigation inside MainActivity

--- app/src/main/java/com/example/weather/ui/MainActivity.kt
+++ app/src/main/java/com/example/weather/ui/MainActivity.kt
@@ -1,20 +1,28 @@
-package com.example.weather
+package com.example.weather.ui
 
 import ApiKey
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.weather.R
 import com.example.weather.data.City
-import com.example.weather.ui.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val weatherViewModel: WeatherViewModel by viewModels()
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
         val apiKey = ApiKey.KEY
         weatherViewModel.fetchWeatherData(City("Nizhniy Novgorod"), apiKey)
